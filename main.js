fe8c82491dee8a6ba84f60c26adc5c83ad4ac321
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-06-07 22:40:05 +0000

    removes useless applyStyle(), implements .css method, moves static styling to stylesheet

--- main.js
+++ main.js
@@ -13,67 +13,47 @@ const drawBarChart = (data, options, element) => {
     xLabel,
     yLabel,
   } = options;
-  let axesOffset = width * 0.07;
-
-  const appendStyle = (element, attribute, value) => {
-    if (!element.attr("style")) {
-      element.attr("style", attribute + ":" + value + ";");
-    } else {
-      element.attr(
-        "style",
-        element.attr("style") + " " + attribute + ":" + value + ";"
-      );
-    }
-  };
+  let AXIS_OFFSET = width * 0.07;
 
   let chart = element;
-  appendStyle(chart, "height", height + "px");
-  appendStyle(chart, "width", width + "px");
-  appendStyle(chart, "border", "thin solid black");
+  chart.css("height", height + "px").css("width", width + "px");
 
   let elementIds = ["x-axis", "y-axis", "title", "scale", "x-label", "y-label"];
   for (let id of elementIds) {
     chart.append("<div id='" + id + "'></div>");
   }
 
-  let titleElement = $("#title").text(title);
-  appendStyle(titleElement, "font-size", titleSize + "px");
-  appendStyle(titleElement, "top", height * 0.01 + "px");
-  appendStyle(
-    titleElement,
-    "left",
-    width / 2 - titleElement.width() / 2 + "px"
-  );
+  let titleElement = $("#title");
+  titleElement
+    .text(title)
+    .css("font-size", titleSize + "px")
+    .css("color", titleColor)
+    .css("left", width / 2 - titleElement.width() / 2 + "px");
 
   let xAxis = $("#x-axis");
-  appendStyle(xAxis, "height", "1px");
-  appendStyle(xAxis, "width", width - axesOffset + "px");
-  appendStyle(xAxis, "background", "black");
-  appendStyle(xAxis, "top", height - axesOffset + "px");
-  appendStyle(xAxis, "left", axesOffset + "px");
+  xAxis
+    .css("width", width - AXIS_OFFSET + "px")
+    .css("top", height - AXIS_OFFSET + "px")
+    .css("left", AXIS_OFFSET + "px");
 
   let yAxis = $("#y-axis");
-  appendStyle(yAxis, "height", height - axesOffset + "px");
-  appendStyle(yAxis, "width", "1px");
-  appendStyle(yAxis, "background", "black");
-  appendStyle(yAxis, "top", 0 + "px");
-  appendStyle(yAxis, "left", axesOffset + "px");
+  yAxis
+    .css("height", height - AXIS_OFFSET + "px")
+    .css("left", AXIS_OFFSET + "px");
 
   let yLabelElement = $("#y-label").text(yLabel);
-  appendStyle(
-    yLabelElement,
+  yLabelElement.css(
     "top",
-    (height - axesOffset) / 2 + yLabelElement.width() / 2 + "px"
+    (height - AXIS_OFFSET) / 2 + yLabelElement.width() / 2 + "px"
   );
-  appendStyle(yLabelElement, "left", "5px");
 
   let xLabelElement = $("#x-label").text(xLabel);
-  appendStyle(xLabelElement, "top", height - xLabelElement.height() - 5 + "px");
-  appendStyle(
-    xLabelElement,
-    "left",
-    axesOffset + (width - axesOffset) / 2 - xLabelElement.width() / 2 + "0px"
-  );
+  xLabelElement
+    .css("top", height - xLabelElement.height() - 5 + "px")
+    .css(
+      "left",
+      AXIS_OFFSET + (width - AXIS_OFFSET) / 2 - xLabelElement.width() / 2 + "px"
+    );
 
   let scale = $("#scale");
 
@@ -86,9 +66,9 @@ $(() => {
   let options = {
     width: 800,
     height: 500,
-    title: "Bar Chart",
+    title: "Vegetables Bought",
     titleSize: 25,
-    titleColor: "black",
+    titleColor: "blue",
     barValuePosition: "top", // top, middle, bottom
     barColor: "red",
     barLabelColor: "black",
